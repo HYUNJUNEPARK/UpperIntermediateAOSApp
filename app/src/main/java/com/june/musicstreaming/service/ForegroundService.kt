1f6d0c7b895de5c542f0535b4cd598f34b98539a
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-06-13 09:15:43 +0900

    notification UI and foreground service

--- app/src/main/java/com/june/musicstreaming/service/ForegroundService.kt
+++ app/src/main/java/com/june/musicstreaming/service/ForegroundService.kt
@@ -4,10 +4,20 @@ import android.app.Service
 import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
+import android.util.Log
+import com.june.musicstreaming.model.NowPlayingMusicModel.Companion.nowPlayingMusic
 
 class ForegroundService : Service() {
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Notification(this).notifyNotification()
+        val artist = nowPlayingMusic?.artist.toString()
+        val title = nowPlayingMusic?.track.toString()
+
+        Notification(this).notifyNotification(artist, title)
+
+        //TODO 음악 컨트롤 할 것 -> 알람에 UI 세팅 먼저
+        //TODO 이 부분이 두번 실행되는 버그 있음 찾아서 수정할 것
+        Log.d("testLog", "onStartCommand: aaaaaaaaaaa")
+
         return super.onStartCommand(intent, flags, startId)
     }
 
--- app/src/main/java/com/june/musicstreaming/service/Notification.kt
+++ app/src/main/java/com/june/musicstreaming/service/Notification.kt
@@ -5,19 +5,20 @@ import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
+import com.june.musicstreaming.R
 import com.june.musicstreaming.service.Constant.Companion.CHANNEL_ID
 import com.june.musicstreaming.service.Constant.Companion.CHANNEL_NAME
 import com.june.musicstreaming.service.Constant.Companion.NOTIFICATION_ID
-import com.june.musicstreaming.service.Constant.Companion.NOTIFICATION_TITLE
 import com.june.musicstreaming.service.Constant.Companion.PLAYER_INTENT_ACTION
 
 class Notification(private val context: Context) {
     private val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
     lateinit var builder: NotificationCompat.Builder
 
-    fun notifyNotification() {
-        notification()
+    fun notifyNotification(artist: String, title: String) {
+        notification(artist, title)
         notificationManager.notify(
             NOTIFICATION_ID,
             builder.build()
@@ -28,16 +29,22 @@ class Notification(private val context: Context) {
         notificationManager.cancel(NOTIFICATION_ID)
     }
 
-    private fun notification() {
+    private fun notification(artist: String, title: String) {
         val intent = Intent(context, ForegroundService::class.java)
         intent.action = PLAYER_INTENT_ACTION
 
+        val contentView = RemoteViews(context.packageName, R.layout.notification)
+        contentView.setImageViewResource(R.id.coverImage, R.mipmap.ic_launcher)
+        contentView.setTextViewText(R.id.title, "$title")
+        contentView.setTextViewText(R.id.artist, "$artist")
+
         builder = notificationBuilder().apply {
             setSmallIcon(android.R.drawable.ic_notification_overlay)
-            setContentTitle(NOTIFICATION_TITLE)
             //User can not cancel notification
             setAutoCancel(false) //block touch cancel
             setOngoing(true) //block swipe cancel
+            //custom notification
+            setContent(contentView)
         }
     }
 
