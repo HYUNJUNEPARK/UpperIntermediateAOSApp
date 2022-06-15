877175208921311f9974dbb5ab237552bb2445b2
HYUNJUNEPARK <june2ac4dev@gmail.com>
2022-06-16 08:55:39 +0900

    table animation effect

--- app/src/main/java/com/june/ott/MainActivity.kt
+++ app/src/main/java/com/june/ott/MainActivity.kt
@@ -1,17 +1,49 @@
 package com.june.ott
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.MotionLayout.TransitionListener
 import androidx.databinding.DataBindingUtil
 import com.june.ott.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private var isGateringMotionAnimating: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.gatheringDigitalTingsMotionLayout.setTransitionListener(object : TransitionListener {
+            override fun onTransitionStarted(motionLayout: MotionLayout?, startId: Int, endId: Int) {
+                isGateringMotionAnimating = true
+            }
+            override fun onTransitionChange(motionLayout: MotionLayout?, startId: Int, endId: Int, progress: Float) { }
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                isGateringMotionAnimating = false
+            }
+            override fun onTransitionTrigger(motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float) { }
+        })
+
+        binding.scrollView.viewTreeObserver.addOnScrollChangedListener {
+            val scrolledValue = binding.scrollView.scrollY
+
+            if (scrolledValue > 150f.dpToPx(this@MainActivity).toInt()) {
+                if (isGateringMotionAnimating.not()) {
+                    binding.gatheringDigitalTingsMotionLayout.transitionToEnd()
+                }
+            } else {
+                if (isGateringMotionAnimating.not()) {
+                    binding.gatheringDigitalTingsMotionLayout.transitionToStart()
+                }
+            }
+        }
 
     }
 }
+
+fun Float.dpToPx(context: Context): Float =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, context.resources.displayMetrics)
